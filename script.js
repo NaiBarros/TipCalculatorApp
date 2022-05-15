227e7b328dc153739a272c4ae73b84d77e10cf8f
NaiBarros <naiara.b.barros@gmail.com>
2022-05-15 20:57:27 -0300

    Finalizando lógica

--- script.js
+++ script.js
@@ -1,49 +1,105 @@
 //elementos
-let billElement = document.getElementById("bill")
-let customTipElement = document.getElementById("custom")
-let numPeopleElement = document.getElementById("people")
-let tipButtons = document.getElementsByClassName("tip-button")
-let errorElement = document.getElementById("error")
+let billElement = document.getElementById("bill");
+let customTipElement = document.getElementById("custom");
+let numPeopleElement = document.getElementById("people");
+let tipButtons = document.getElementsByClassName("tip-button");
+let errorElement = document.getElementById("error");
+let tipAmountElement = document.getElementById("tip-amount");
+let totalElement = document.getElementById("total");
+let customElement = document.getElementById("custom");
 
 //variáveis
-let bill = 0
-let tip = 0
-let numPeople = null
+let bill = 0;
+let tip = 0;
+let numPeople = 0;
+let tipAmount = 0;
+let total = 0;
 
 billElement.addEventListener("input", (event) => {
-  bill = event.target.value
-})
+  bill = event.target.value;
+  calculateValues();
+});
 
 numPeopleElement.addEventListener("input", (event) => {
-  numPeople = event.target.value
+  numPeople = event.target.value;
+  calculateValues();
 
   if (numPeople != 0) {
-    numPeopleElement.classList.remove("input-error")
-    return errorElement.classList.remove("visible")
+    numPeopleElement.classList.remove("input-error");
+    return errorElement.classList.remove("visible");
   }
 
-  numPeopleElement.classList.add("input-error")
-  errorElement.classList.add("visible")
-})
+  numPeopleElement.classList.add("input-error");
+  errorElement.classList.add("visible");
+});
 
-function setTip(tipValue) {
-  tip = tipValue
+customElement.addEventListener("input", (event) => {
+  if (event.target.value > 100) {
+    event.target.value = 100;
+  }
+  setTip(event.target.value, true);
+});
 
-  let tipButtonsArray = Array.from(tipButtons) //ou = [...tipButtons]
+function setTip(tipValue, useCustomElement = false) {
+  tip = tipValue;
+  calculateValues();
+
+  let tipButtonsArray = Array.from(tipButtons); //ou = [...tipButtons]
+
+  if (useCustomElement) {
+    for (let button of tipButtonsArray) {
+      button.classList.remove("selected");
+    }
+
+    return;
+  }
+
+  customElement.value = null;
 
   let tipButton = tipButtonsArray.find((button) => {
-    return button.dataset.value == tipValue
-  })
+    return button.dataset.value == tipValue;
+  });
 
-  for (let button of tipButtonsArray) { //para cada botão do tipButtonsArray
+  for (let button of tipButtonsArray) {
+    //para cada botão do tipButtonsArray
     if (button != tipButton) {
-      button.classList.remove("selected")
+      button.classList.remove("selected");
     }
   }
 
-  tipButton.classList.add("selected")
+  tipButton.classList.add("selected");
 }
 
+function calculateValues() {
+  if (bill == 0 || tip == 0 || numPeople == 0) {
+    return;
+  }
 
+  let percentage = tip / 100;
+  let tipTotal = percentage * bill;
+  tipAmount = tipTotal / numPeople;
+  total = tipAmount + bill / numPeople;
 
+  tipAmountElement.innerText = `$${tipAmount.toFixed(2)}`;
+  totalElement.innerText = `$${total.toFixed(2)}`;
+}
 
+function resetValues() {
+  bill = 0;
+  tip = 0;
+  numPeople = 0;
+  tipAmount = 0;
+  total = 0;
+
+  tipAmountElement.innerText = "$0.00";
+  totalElement.innerText = "$0.00";
+  billElement.value = null;
+  numPeopleElement.value = null;
+  customElement.value = null;
+
+  let tipButtonsArray = Array.from(tipButtons);
+
+  for (let button of tipButtonsArray) {
+    button.classList.remove("selected");
+  }
+}
